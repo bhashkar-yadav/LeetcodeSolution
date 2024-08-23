b43bb5b40fe8f5723c0276ea74d81efaa4636f43
Bhashkar Yadav <116943212+bhaskar-5625@users.noreply.github.com>
2024-08-23 09:18:57 +0530

    Covert Binary to Decimal and Decimal to Binary

---
+++ numberSystem.cpp
@@ -0,0 +1,36 @@
+//Binary to Decimal
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int result=0;
+//     int power=1;
+//     while(n!=0){
+//         int ld=n%10;
+//         result+=ld*power;
+//         power*=2;
+//         n=n/10;
+//     }
+//     cout<<result;
+//     return 0;
+// }
+
+
+//Decimal to Binary
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int result=0;
+//     int power=1;
+//     while(n!=0){
+//         int parityDigit=n%2;
+//         result+=parityDigit*power;
+//         power*=10;
+//         n=n/2;
+//     }
+//     cout<<result;
+//     return 0;
+// }
